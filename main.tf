5a706216f6315ba686e0cbabf7f832c5920324f8
Pollák Matúš <mpollak@trask.cz>
2021-08-13 17:00:11 +0200

    initial configuration

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.48.0"
+    }
+  }
+}
+provider "azurerm" {
+    features {}
+  
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "Aks-terraform"
+  location = "westeurope"
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "Devops AKS"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "Devopsaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "2"
+    vm_size    = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
